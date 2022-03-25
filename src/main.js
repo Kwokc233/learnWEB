a4e282b03117f2bde91ea8547017aaca911be3c4
ame <1765074429@qq.com>
2022-03-25 21:06:22 +0800

    only_add_vueconfigjs_can_work

--- src/main.js
+++ src/main.js
@@ -5,11 +5,101 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
-
+Vue.component('todo-item', {
+  template: '<li>这是个待办项</li>'
+})
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+
+var app = new Vue({
+  el:'app',
+  data:{
+    message:'hello'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: '页面加载于 ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: '学习 JavaScript' },
+      { text: '学习 Vue' },
+      { text: '整个牛项目' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: '蔬菜' },
+      { id: 1, text: '奶酪' },
+      { id: 2, text: '随便其它什么人吃的东西' }
+    ]
+  }
+})
+
+var data = { a: 1 }
+// 该对象被加入到一个 Vue 实例中
+var vm = new Vue({
+  data: data
+})
+
+var obj = {
+  foo: 'bar'
+}
+Object.freeze(obj)
+var app0=new Vue({
+  el: '#app0',
+  data: obj
+})
+
+var data1 = { a: 1 }
+var vm = new Vue({
+  el: '#example',
+  data: data1
+})
+vm.$data === data1 // => true
+vm.$el === document.getElementById('example') // => true
+// $watch 是一个实例方法
+vm.$watch('a', function (newValue, oldValue) {
+  // 这个回调将在 `vm.a` 改变后调用
 })
